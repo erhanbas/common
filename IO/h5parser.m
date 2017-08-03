4054a9298e8702295c754b0e1c4e9ebd607b81ed
Erhan Bas <erhanbas@gmail.com>
2017-08-03 12:49:30 -0400

    added h5parser for faster field retriaval

---
+++ IO/h5parser.m
@@ -0,0 +1,22 @@
+function [outputArgs] = h5parser(inputArgs)
+%H5PARSER retrive related fields from a large h5 file. using h5info is
+%exteremly slow, especialy if the file is hosted on a shared drive
+% 
+% [OUTPUTARGS] = H5PARSER(INPUTARGS) Explain usage here
+% 
+% Inputs: 
+% 
+% Outputs: 
+% 
+% Examples: 
+% 
+% Provide sample usage code here
+% 
+% See also: List related files here
+
+% $Author: base $	$Date: 2017/08/03 12:47:24 $	$Revision: 0.1 $
+% Copyright: HHMI 2017
+
+
+
+end
